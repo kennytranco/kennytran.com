1e0da0f2b2f8678dbb321d75a8317bea7889d788
Kenny Tran <kenny@kennytran.com>
2021-01-16 21:48:06 +0000

    Fix post-excerpt tags and spacing

--- wp-content/themes/kennytran/template-parts/content/post-excerpt.php
+++ wp-content/themes/kennytran/template-parts/content/post-excerpt.php
@@ -1,7 +1,7 @@
 <article class="c-post-excerpt">
     <time class="c-post-excerpt__date" datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date('F jS, Y'); ?></time>
     <h2 class="c-post-excerpt__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php $tags = get_tags(array('hide_empty' => true)); ?>
+    <?php $tags = wp_get_post_terms($post->ID, 'post_tag', array('fields' => 'all')); ?>
     <?php if(!empty($tags)) : ?>
         <ul class="c-post-excerpt__tags">
             <?php foreach($tags as $tag) : ?>
